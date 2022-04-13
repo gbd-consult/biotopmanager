8f22c798e640f6db308a4d7ae7d19abf369e9115
felixfeckler <felix.feckler@gmx.de>
2022-04-13 17:11:49 +0200

    cleanup adminref

--- adminref/skripte/DELETE.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Löschen der Datenbank mit allen Benutzern, ausführen als Benutzer postgres
-
-dropdb biotope
-dropuser biotope_admin
-dropuser biotope_editor
-
--- adminref/skripte/import_biotopdaten.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Import Biotopdaten mit ogr2ogr
-# USAGE: bash import_biotopdaten.sh </pfad/zu/biotop_25833_all.geojson>
-
-ogr2ogr -lco OVERWRITE=YES -progress -nln biotope -lco SCHEMA=biotope \
--nlt PROMOTE_TO_MULTI -geomfield wkb_geometry \
--f "PostgreSQL" PG:"dbname=biotope user=biotope_admin password=biotope_admin" ${1}
